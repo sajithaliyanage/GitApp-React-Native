fbd2297f568fd1b9d4d0505ba924981b8101424b
sajithaliyanage <sajithaliyanage@gmail.com>
2017-05-26 14:38:25 +0530

    Init app

---
+++ index.android.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+import {Header,Left,Right,Icon,Body,Title, Container, Content, Button, Text } from 'native-base';
+
+export default class app extends Component {
+  render() {
+    return (
+
+      <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Header</Title>
+                    </Body>
+                    <Right>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Right>
+                </Header>
+            </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('app', () => app);
